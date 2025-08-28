009e1df5c80f6fbb2697c605ec696b6f812619a7
Yewon Kim <giveresteve@gmail.com>
2025-08-28 10:35:33 -0600

    Update tb_alu.v

--- tb_alu.v
+++ tb_alu.v
@@ -23,31 +23,12 @@ module alutest;
 	);
 
 	initial begin
-			$monitor("A: %0d, B: %0d, C: %0d, Flags[1:0]:%b, time:%0d", A, B, C, Flags[1:0], $time );
-
-//Instead of the $display stmt in the loop, you could use just this
-//monitor statement which is executed everytime there is an event on any
-//signal in the argument list.
-
-		//SPECIFIC AND CORNER CASE TESTS:
+		$monitor("A: %0d, B: %0d, C: %0d, Flags[1:0]:%b, time:%0d", A, B, C, Flags[1:0], $time );
 		
-		// Initialize Inputs
-		//A = 0;
-		//A = 0;
-		//B = 0;
-		//Opcode = 2'b11;
-
-		// Wait 100 ns for global reset to finish
-/*****
-		// One vector-by-vector case simulation
-		//#10;
-	        //Opcode = 2'b11;
-		//A = 4'b0010; B = 4'b0011;
-		//#10
-		//A = 4'b1111; B = 4'b 1110;
-		//$display("A: %b, B: %b, C:%b, Flags[1:0]: %b, time:%d", A, B, C, Flags[1:0], $time);
-****/
-		A = 16'h0000; B = 16'h0000; Opcode = 31; #10; // NOP
+		A = 16'h0000; 
+		B = 16'h0000; 
+		Opcode = uut.ADD; // Need to change based on the default Opcode
+		#10; 
 
 		// 1) ADD (signed) overflow: 0x7FFF + 1 = 0x8000  => F=1, C=0, N=1, Z=0
     	Opcode = uut.ADD;  A = 16'h7FFF; B = 16'h0001; #10;
