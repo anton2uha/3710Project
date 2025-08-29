ea5159faa3e99d6756d70d6f24b4afb063e8c218
iansmith1112 <u0969005@utah.edu>
2025-08-29 17:11:23 -0600

    Most instructions implemented
    This commit is a lot of the work for the ALU, but there is still work to be done. Opcodes need to be assigned, and work needs to be checked. I decided to implement heavily following the CR16a ref sheet (see readme).

---
+++ README.md
@@ -0,0 +1,7 @@
+# 3710 Project
+
+## ALU Instructions
+
+closely follows implementation listed [here](https://my.eng.utah.edu/~cs3710/handouts/cr16a-prog-ref.pdf)
+
+Destination input = A, Src input = B. This means CMP does A<B and SUB does A-B.
--- alu.v
+++ alu.v
@@ -21,20 +21,16 @@ OR, XOR, NOT, LSH, LSHI, RSH, RSHI, ALSH, ARSH, NOP/WAIT
 parameter ADD   = 4'b0101;
 parameter ADDU  = 4'b0110;
 parameter ADDC  = 4'b0111;
-parameter ADDCU = 4'b0100; // 1st unused OPCODE
 parameter SUB   = 4'b1001;
 parameter SUBC  = 4'b1010;
 parameter CMP   = 4'b1011;
-parameter CMPU  = 4'b????; // <------
 parameter AND   = 4'b0001;
 parameter OR    = 4'b0010;
 parameter XOR   = 4'b0011;
 parameter MOV   = 4'b1101;
 parameter LSH   = 4'b0100;
 parameter NOT   = 4'b????; // <------
-parameter RSH   = 4'b????; // <------
-parameter ALSH  = 4'b????; // <------
-parameter ARSH  = 4'b????; // <------
+parameter ASHU  = 4'b????; // <------
 parameter NOP   = 4'b????; // <------
 
 
@@ -45,41 +41,37 @@ parameter NOP   = 4'b????; // <------
 	Flags[4,3,2,1,0] = Zero(Z), Carry(C), Overflow(O), ?Low(L), ?Negative(N)
 */
 
-always @(A, B, Opcode)
+always @(A, B, cin, Opcode)
 begin
 	case (Opcode)
-	ADDU:
+	ADDU: // does not set flags
 		begin
 		// Simply add A and B, no need to touch flags as detailed in CR16a manual
 		C = A + B;
 		end
-	ADD:
+	ADD: // sets carry and overflow flags
 		begin
 
 		// Reset flags
 		Flags = 5'b00000;
 		// Sum A and B, and also set the carry flag if a carry happens.
 		{Flags[3], C} = A + B;
-		// If the sum is zero, set the Zero flag (Regardless if carry happened)
-		if (C == 16'b0) Flags[4] = 1'b1;
 		// Set overflow flag
 		if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) ) Flags[2] = 1'b1;
 
 		end
-	ADDC:
+	ADDC: // sets carry and overflow flags
 		begin
 
 		// Reset flags
 		Flags = 5'b00000;
 		// Sum A, B, cin, and also set the carry flag if a carry happens.
 		{Flags[3], C} = A + B + cin;
-		// If the sum is zero, set the Zero flag (Regardless if carry happened)
-		if (C == 16'b0) Flags[4] = 1'b1;
 		// Set overflow flag
 		if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) ) Flags[2] = 1'b1;
 
 		end
-	SUB:
+	SUB: // sets carry and overflow flags
 		begin
 
 		// Reset flags
@@ -88,13 +80,10 @@ begin
 		C = A - B;
 		// Set the carry flag (A<B)
 		if (A < B) Flags[3] = 1'b1;
-		// If C is zero, set the Zero flag
-		if (C == 16'b0) Flags[4] = 1'b1;
 		// If overflow happened, set the overflow flag
 		if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) ) Flags[2] = 1'b1;
-
 		end
-	SUBC:
+	SUBC: // sets carry and overflow flags
 		begin
 
 		// Reset flags
@@ -103,53 +92,72 @@ begin
 		C = A - B - cin;
 		// Set the carry flag (A<B+cin)
 		if (A < (B+cin)) Flags[3] = 1'b1;
-		// set zero flag
-		if (C == 4'b0000) Flags[4] = 1'b1;
 		// set overflow flag
 		if( (~A[15] & ~B[15] & C[15]) | (A[15] & B[15] & ~C[15]) ) Flags[2] = 1'b1;
 
 		end
-	CMP: // TODO
+	CMP: // sets zero, low, negative flags
 		begin
-		if( $signed(A) < $signed(B) ) Flags[1:0] = 2'b11;
-		else Flags[1:0] = 2'b00;
-		C = 4'b0000;
-		Flags[4:2] = 3'b000;
-		// both positive or both negative
-		/*if( A[3] == B[3] )
-		begin
-			if (A < B) Flags[1:0] = 2'b11;
-			else Flags[1:0] = 2'b00;
-		end
-		else if (A[3] == 1'b0) Flags[1:0] = 2'b00;
-		else Flags[1:0] = 2'b01;
-		Flags[4:2] = 3'b000;
-		
-		// C = ?? if I don;t specify, then I'm in trouble.
-		C = 4'b0000;
-		*/
-		end
-	CMPU: // TODO
-		begin
-		if($unsigned(A)<$unsigned(B)) Flags[1:0] = 2'b11;
-		else flags[1:0] = 2'b00;
-		C=4'b0000;
+
+		// Reset flags
+		Flags = 5'b00000;
+		// If A==B is zero, set the Zero flag
+		if (A == B) Flags[4] = 1'b1;
+		// Set the negative flag if A < B (signed)
+		if ($signed(A) == $signed(B)) Flags[0] = 1'b1;
+		// Set the low flag if A < B (unsigned)
+		if ($unsigned(A) < $unsigned(B)) Flags[1] = 1'b1;
 
 		end
-	AND:
-	OR: 
-	XOR:
-	MOV:
-	LSH:
-	NOT:
-	RSH:
-	ALSH:
-	ARSH:
-	NOP:
-	default: 
+	AND: // does not set flags
 		begin
+
+		C = A & B;
+
+		end
+	OR: 
+		begin
+
+		C = A | B;
+
+		end
+	XOR:
+		begin
+
+		C = A ^ B;
+
+		end
+	//MOV: Without a reg file I am not sure how to implement this
+	LSH: // does not set flags. Also supports negative shifts (right shifts) per CR16a manual
+		begin
+        if ($signed(B) >= 0)
+            C = A << B;
+        else // If B is negative, do a logical right shift instead
+            C = A >> (-$signed(B));
+        end	
+	NOT:
+		begin
+
+		C = ~A;
+
+		end
+	ASHU: // Arithmetic Shift. Does not set flags
+
+		begin
+		if ($signed(B) >= 0)
+			C = A << B;
+		else // If B is negative, do a arithmetic right shift instead (preserves sign bit)
+			C = ($signed(A) >>> (-$signed(B)));
+
+		end
+	NOP: begin end // No operation, do nothing
+
+	default: // undefined behavior
+		begin
+
 			C = 4'b0000;
 			Flags = 5'b00000;
+
 		end
 	endcase
 end
