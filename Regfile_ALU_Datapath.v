3aba8f129c13cd8cf8edf701f33c35263844c48f
anton2uha <anton2uha@gmail.com>
2025-09-08 00:20:07 -0600

    add to FSM

--- Regfile_ALU_Datapath.v
+++ Regfile_ALU_Datapath.v
@@ -44,32 +44,37 @@ reg [3:0] raddrB;  //rsrc
 wire [15:0] regFileInput;
 wire [15:0] rdataA;
 wire [15:0] rdataB;
-wire [1:0] regFileWriteEnable;
+reg [1:0] regFileWriteEnable;
 
 //ALU connections
 wire [15:0] dataA;
 wire [15:0] dataB;
 wire [15:0] ALUout;
-wire regToAluMuxEnable; //maybe need seperate enables for A and B?
+reg dataAMuxEnable; //maybe need seperate enables for A and B?
+reg [1:0] immediateEnable;
 
 
 //extract info from instr. Below works for most but not all instrs, like shifts, will come back.
 reg [3:0] Opcode;
-reg [7:0] Imm;
+reg [7:0] imm;
 always @(*) begin
     if (instr[15:12] == 4'b0000)
         Opcode = instr[7:4];
 		  raddrB = instr[3:0];
-		  Imm = 8'b0;
+		  imm = 8'b0;
+		  immediateEnable = 2'b00;
     else
         Opcode = instr[15:12];
 		  raddrB = 4'b0;
-		  Imm = instr[7:0];
+		  imm = instr[7:0];
+		  immediateEnable = 2'b01;
 end
 
 
+
+
 mux muxA (rdataA, regToAluMuxEnable, dataA);
-mux muxB (rdataB, regToAluMuxEnable, dataB);
+twoToOneMux immMux (rdataB, imm, immediateEnable, dataB);
 twoToOneMux muxRegFile (inData, ALUout, regFileWriteEnable, regFileInput);
 
 regfile my_regs
@@ -102,39 +107,56 @@ parameter FETCH = 2'b001;  //fetch: read instr from mem, we feed data directly f
 parameter DECODE = 2'b010; //decode: decode instr, update regfile
 parameter EXECALU = 2'b011;  //execute ALU
 parameter WRITEBACK = 2'b100; //writeback: place output of ALU into regfile
-parameter NEXT = 2'b101; //next: updates progam counter, preps for next fetch. Not sure if we need this now?
 
 reg [2:0] state;
+//this loop takes care of state: FETCH -> DECODE -> EXECUTE -> WRITEBACK -> FETCH
+always @(posedge clk or posedge reset) begin
+    if (reset)
+        state <= FETCH;       // start in FETCH after reset
+    else begin
+        case (state)
+            FETCH:     state <= DECODE;
+            DECODE:    state <= EXECUTE;
+            EXECUTE:   state <= WRITEBACK;
+            WRITEBACK: state <= FETCH;   // loop back
+            default:     state <= FETCH;   // safety
+        endcase
+    end
+end
+
+//this loop executes logic for current state.
 always @(posedge clk, posedge reset) begin
 	if (reset) begin
 		state <= RESET;
-		regFileInputEnable <= 2'b11;
+		regFileInputEnable <= 2'b00;
 	end else begin
 		case (state)
 			FETCH: begin
-				
-			
+				regFileWriteEnable = 2'b01; //dataIn
+				dataAMuxEnable = 1'b0;
+				immediateEnable = 1'b00;
 			end
 			
 			DECODE: begin
-				
-			
+				regFileWriteEnable = 2'b11;
+				dataAMuxEnable = 1'b0;
+				immediateEnable = 1'b11;
 			end
 			
 			EXECALU: begin
-			
+				
 			end
 			
 			WRITEBACK: begin
-			
-			end
-			
-			NEXT: begin
-			
+				regFileWriteEnable = 2'b10; //ALUout
+				dataAMuxEnable = 1'b0;
+				immediateEnable = 1'b00;
 			end
 			
 			default: begin
-			
+				regFileWriteEnable = 2'b00;
+				dataAMuxEnable = 1'b0;
+				immediateEnable = 1'b00;
 			end
 		
 		endcase
