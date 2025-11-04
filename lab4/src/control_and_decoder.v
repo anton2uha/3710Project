26bb130ae5403328661171010bbda0ec35f118f6
iansmith1112 <u0969005@utah.edu>
2025-11-03 19:38:25 -0700

    tried to account for driving a constant output once we have complete our instructions we want to execute

--- lab4/src/control_and_decoder.v
+++ lab4/src/control_and_decoder.v
@@ -65,78 +65,96 @@ module control_and_decoder(
 
     reg [4:0] state, next_state, prev_state;
 
+    integer i = 0;
+    parameter instrs = 1;
+
+
     //this loop takes care of state
     always @(posedge clk or negedge reset) begin
-        // if (!reset) begin
-        //     state      <= S0;
-        //     prev_state <= S0; // initialize previous state on reset for clean capture
-        // end
-        // else begin
-        //     prev_state <= state;
-        //     state      <= next_state;
-        // end
-        case (state)
-            S0: state <= S1;
-            S1: state <= S2;
-            S2: state <= S3;
-            S3: state <= S0;
-            default: state <= S0;   // safety
-        endcase
+        if(!reset_n) begin
+            state <= S0;
+        end
+        else begin 
+            case (state)
+                S0: state <= S1;
+                S1: state <= S2;
+                S2: begin
+                    if(i == insts) state <= S2;
+                    else state <= S0;
+                end
+                default: state <= S0;   // safety
+            endcase
+        end
     end
 
     //this loop executes logic for current state
     always @(*) begin
-        // output reg         pc_en,        // to do PC+1 
-        // output reg         ir_en,        // IR <= DOUT during S1
-        // output reg         reg_we,       // regfile write enable
-        // output reg         imm_en,       // 0: B=Rdest (RR), 1: B=Imm 
-        // output reg  [3:0]  op,           // opcode (mapped from instr[12:9])
-        // output reg  [3:0]  rsrc,         // src reg index  (instr[4:1])
-        // output reg  [3:0]  rdest,        // dest reg index (instr[8:5])
-        // output reg  [7:0]  imm8,        
-        // output reg  [15:0] reg_en,
-        // Initialize all values to prevent latches
-        pc_en = 1;
-        ir_en = 0;
-        reg_we = 0;
-        imm_en = 0;
-        op = 4'd0;
-        rsrc = 4'd0;
-        rdest = 4'd0;
-        reg_en = 16'd0;
-        imm8   = 8'd0; 
-
         case (state)
             S0: begin
+                pc_en = 1;
+                ir_en = 0;
+                reg_we = 0;
+                imm_en = 0;
+                rsrc = 0;
+                rdest = 0;
+                op = 4'd0;
+                reg_en = 16'd0;
+                imm8   = 8'd0; 
                 // TODO: Fetch stage, what needs to happen in here?
                 pc_en = 0;
             end
             S1: begin
+                pc_en = 0;
+                reg_we = 0;
+                imm_en = 0;
+                reg_en = 16'd0;
+
                 // opcode = instr[15:12];
                 // rd     = instr[11:8];
                 // ext    = instr[7:4];
                 // rs     = instr[3:0];
-                // imm    = instr[7:0];
+                imm8   = instr[7:0];
+                rdest  = instr[11:8];
+                rsrc   = instr[3:0];
 
+                // IR cntl
                 ir_en = 1;
 
                 if(instr[15:12] != 4'b0000) begin
                     op = instr[15:12];
                 end
-                else begin op = instr[7:4] end
-
-                // set imm cntl
-                if(opcode == 4'b0101) begin
-                    imm_en = 1;
+                else begin 
+                    op = instr[7:4];
+                    // set imm cntl
+                    imm_en = 1; 
                 end
-                // TODO: IR cntl
             end
             S2: begin
-                // TODO: reg_we & reg_en
+                imm_en = 0;
+                reg_en = 16'd0;
+                imm8   = instr[7:0];
+                rdest  = instr[11:8];
+                rsrc   = instr[3:0];
+
+                if(instr[15:12] != 4'b0000) begin
+                    op = instr[15:12];
+                end
+                else begin 
+                    op = instr[7:4];
+                    // set imm cntl
+                    imm_en = 1; 
+                end
+                
+
+                // IR cntl
+                ir_en = 1;
+
+                // reg_we & reg_en
                 // Convert rd into a 16 bit value for reg_en. Ex: if rd is 4 then reg_en would be 0000000000010000
                 reg_en = 16'd1 << rdest;
+                reg_we = 1;
 
-                // TODO: pc
+                // pc
                 pc_en = 1;
             end
         endcase
